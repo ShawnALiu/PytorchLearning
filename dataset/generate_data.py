d089c6872d00a7ed669f597050b66bec95c69b8a
刘振 <547858171@qq.com>
2024-05-27 20:49:28 +0800

    alexnet在cifar10上训练

--- dataset/generate_data.py
+++ dataset/generate_data.py
@@ -18,21 +18,31 @@ def generate_linear_data():
     return x_train, y_train
 
 
-def generate_cifar10():
+def generate_cifar10(shuffle=False, show_data=False):
+    transform = transforms.Compose([
+        transforms.RandomResizedCrop((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+
     cifar10_dataset = torchvision.datasets.CIFAR10(root='../data',
                                                    train=False,
-                                                   transform=transforms.ToTensor(),
+                                                   transform=transform,
                                                    target_transform=None,
                                                    download=True)
     # 取32张图片的tensor
-    tensor_dataloader = DataLoader(dataset=cifar10_dataset, batch_size=256)
-    for index, (img_tensor, label_tensor) in enumerate(tensor_dataloader):
-        if index >= 1:
-            break
-        print(img_tensor.shape)
-        grid_tensor = torchvision.utils.make_grid(img_tensor, nrow=16, padding=2)
-        grid_img = transforms.ToPILImage()(grid_tensor)
-        grid_img.show()
+    tensor_dataloader = DataLoader(dataset=cifar10_dataset, batch_size=32, shuffle=shuffle)
+
+    if show_data:
+        for index, (img_tensor, label_tensor) in enumerate(tensor_dataloader):
+            if index >= 1:
+                break
+            print(img_tensor.shape)
+            grid_tensor = torchvision.utils.make_grid(img_tensor, nrow=16, padding=2)
+            grid_img = transforms.ToPILImage()(grid_tensor)
+            grid_img.show()
+
+    return tensor_dataloader
 
 
 if __name__ == '__main__':
--- fine_tune/train_alexnet.py
+++ fine_tune/train_alexnet.py
@@ -1,13 +1,43 @@
+import numpy as np
 import torch
 import torchvision
+from tqdm import tqdm
 from PIL import Image
 from torchvision import transforms
 
+from dataset import generate_data
+
 
 def main():
+    epochs = 10
+    alexnet = _get_model()
+    train_dataloader = generate_data.generate_cifar10(shuffle=True)
+    optimizer = torch.optim.SGD(alexnet.parameters(), lr=1e-4, weight_decay=1e-2, momentum=0.9)
+    criterion = torch.nn.CrossEntropyLoss()
 
-    alexnet = torchvision.models.alexnet(pretrained=True)
 
+    for epoch in range(1, epochs + 1):
+        loss = train(epoch, train_dataloader, alexnet, optimizer, criterion, epochs)
+
+
+
+def train(epoch, dataloader, model, optimizer, criterion, epochs):
+    losses = []
+    pbar = tqdm(dataloader, ncols=100)
+    for step, item in enumerate(pbar, start=1):
+        input = item[0]
+        target = item[1]
+        output = model(input)
+        loss = criterion(output, target)
+        losses.append(loss.item())
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        pbar.set_description("[{}/{}]TRAIN: STEP_LOSS={:.5f}".format(epoch, epochs, loss.item()))
+
+    return np.mean(losses)
 
 
 @torch.no_grad()
@@ -27,6 +57,25 @@ def test(path):
     print('图片={}，预测的标签为={}'.format(path, pred))
 
 
+# 修改模型，进行微调
+def _get_model():
+    alexnet = torchvision.models.alexnet(pretrained=True)
+    print(alexnet)
+    # (classifier): Sequential(
+    #   ...
+    #   (6): Linear(in_features=4096, out_features=1000, bias=True)
+    # )
+
+    # 将最后一层全连接层改为输出只有10维，因为cifar10只有10类
+    fc_in_features = alexnet.classifier[6].in_features
+    alexnet.classifier[6] = torch.nn.Linear(fc_in_features, 10)
+    print(alexnet)
+    return alexnet
+
+
+
 if __name__ == '__main__':
-    _path = '../data/dog.jpg'
-    test(_path)
+    # _path = '../data/dog.jpg'
+    # test(_path)
+
+    main()
